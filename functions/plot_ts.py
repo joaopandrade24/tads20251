d407e4bc960fb0ace6d16f99ccfab200b73bde20
João Pedro Lyra C. de Andrade <jplyra0508@hotmail.com>
2025-08-18 23:19:38 +0000

    [build]: main app prototype

---
+++ functions/plot_ts.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+import plotly.express as px
+
+def plot (ticker:str):
+
+    data = yf.download(f'{ticker}',  period='max', multi_level_index = False) 
+    df = data.reset_index()[['Date', 'Close']]
+
+    fig = px.line(df, x='Date', y= 'Close', title = f'Histórico de {ticker}')
+
+    return fig
